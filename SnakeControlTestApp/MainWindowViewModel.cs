d2fd05ce49d59c853efcafb58d073e60dc87f387
Borislav Kulov <bkulov@gmail.com>
2015-06-22 15:38:08 +0300

    Initial commit

---
+++ SnakeControlTestApp/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace SnakeControlTestApp
+{
+	public class MainWindowViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public IEnumerable<string> Items
+		{
+			get
+			{
+				for (int i = 0; i < 20; i++)
+				{
+					yield return "Item " + i;
+				}
+			}
+		}
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			var handler = this.PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
