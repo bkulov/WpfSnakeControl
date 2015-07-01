adb5befc8932d989f8ef47d5fa8fe6bd90122b16
Borislav Kulov <bkulov@gmail.com>
2015-07-01 20:31:52 +0300

    Add some dummy unit tests

---
+++ UnitTests/SnakePanelTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfCustomControlsLibrary;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class SnakePanelTests
+	{
+		private SnakeControl snakeControl;
+
+		[TestInitialize]
+		public void Init()
+		{
+			this.snakeControl = new SnakeControl();
+		}
+
+		[TestMethod]
+		public void MeasureOverride_WithoutItems_DesiredSizeShouldBeEmptyRect()
+		{
+			// TODO: test measure override with no items
+
+			Assert.IsTrue(true);
+		}
+		
+		[TestMethod]
+		public void MeasureOverride_WithItems_DesiredSizeShouldNotBeEmptyRect()
+		{
+			this.SetItems(10);
+
+			// TODO: test measure override with items
+
+			Assert.IsTrue(true);
+		}
+
+		// TODO: write more tests to cover the arrange override
+
+		private void SetItems(int count)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				this.snakeControl.Items.Add("test string item " + i);
+			}
+		}
+	}
+}
